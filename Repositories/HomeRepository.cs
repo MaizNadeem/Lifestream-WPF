4387dde65f8fb142b3d7f7a3934cc63a8434b342
MaizNadeem <maiznadeem616@gmail.com>
2023-07-11 02:14:54 +0500

    Making Graphs using LiveCharts.WPF

---
+++ Repositories/HomeRepository.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using WPFApp.Models;
+
+namespace WPFApp.Repositories
+{
+    public class HomeRepository : RepositoryBase
+    {
+        public IEnumerable<BloodTypeGraphData> GetBloodTypeGraphData(DateTime startDate, DateTime endDate, int intervalDays)
+        {
+            string query = @"SELECT GraphDate, BloodType, DonationCount
+                     FROM dbo.GetBloodTypeGraphData(@StartDate, @EndDate, @IntervalDays)";
+
+            using (SqlConnection connection = GetConnection())
+            {
+                connection.Open();
+
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@StartDate", startDate);
+                    command.Parameters.AddWithValue("@EndDate", endDate);
+                    command.Parameters.AddWithValue("@IntervalDays", intervalDays);
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        List<BloodTypeGraphData> bloodTypeGraphData = new List<BloodTypeGraphData>();
+
+                        while (reader.Read())
+                        {
+                            BloodTypeGraphData data = new BloodTypeGraphData
+                            {
+                                GraphDate = reader.GetDateTime(0),
+                                BloodType = reader.GetString(1),
+                                DonationCount = reader.GetInt32(2)
+                            };
+
+                            bloodTypeGraphData.Add(data);
+                        }
+
+                        return bloodTypeGraphData;
+                    }
+                }
+            }
+        }
+
+    }
+}
--- ViewModels/HomeViewModel.cs
+++ ViewModels/HomeViewModel.cs
@@ -1,13 +1,64 @@
-﻿using System;
+﻿using LiveCharts;
+using LiveCharts.Defaults;
+using LiveCharts.Wpf;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using WPFApp.Models;
+using WPFApp.Repositories;
 
 namespace WPFApp.ViewModels
 {
     public class HomeViewModel : ViewModelBase
     {
+        private HomeRepository homeRepository;
 
+        public HomeViewModel()
+        {
+            homeRepository = new HomeRepository();
+
+            SeriesCollection = new SeriesCollection();
+            Labels = new List<string>();
+
+            DateTime startDate = new DateTime(2023, 01, 01);
+            DateTime endDate = new DateTime(2023, 12, 31);
+            int interval = 30;
+
+            // Call the method in HomeRepository to get the blood type graph data
+            List<BloodTypeGraphData> bloodTypeGraphData = homeRepository.GetBloodTypeGraphData(startDate, endDate, interval).ToList();
+
+            // Extract the distinct blood types from the graph data
+            List<string> bloodTypes = bloodTypeGraphData.Select(d => d.BloodType).Distinct().ToList();
+
+            // Create LineSeries for each blood type
+            foreach (string bloodType in bloodTypes)
+            {
+                List<double> donationCounts = bloodTypeGraphData
+                    .Where(d => d.BloodType == bloodType)
+                    .OrderBy(d => d.GraphDate)
+                    .Select(d => (double)d.DonationCount)
+                    .ToList();
+
+                SeriesCollection.Add(new LineSeries
+                {
+                    Title = bloodType,
+                    Values = new ChartValues<double>(donationCounts),
+                    PointGeometry = null,
+                    LineSmoothness = 0.3,
+                });
+            }
+
+            // Get the distinct graph dates
+            List<DateTime> graphDates = bloodTypeGraphData.Select(d => d.GraphDate.Date).Distinct().ToList();
+
+            // Create labels for each graph date
+            Labels = graphDates.Select(date => date.ToString("dd/MM/yyyy")).ToList();
+
+            YFormatter = value => value.ToString("N0");
+        }
+
+        public SeriesCollection SeriesCollection { get; set; }
+        public List<string> Labels { get; set; }
+        public Func<double, string> YFormatter { get; set; }
     }
 }
