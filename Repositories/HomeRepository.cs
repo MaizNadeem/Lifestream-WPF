d7cc33fbe303a191fedd8e446a21b4d599e4bd0a
MaizNadeem <maiznadeem616@gmail.com>
2023-07-14 18:41:39 +0500

    Dashboard Complete

--- Repositories/HomeRepository.cs
+++ Repositories/HomeRepository.cs
@@ -11,7 +11,7 @@ namespace WPFApp.Repositories
         public IEnumerable<BloodTypeGraphData> GetBloodTypeGraphData(DateTime startDate, DateTime endDate, int intervalDays)
         {
             string query = @"SELECT GraphDate, BloodType, DonationCount
-                     FROM dbo.GetBloodTypeGraphData(@StartDate, @EndDate, @IntervalDays)";
+                             FROM dbo.GetBloodTypeGraphData(@StartDate, @EndDate, @IntervalDays)";
 
             using (SqlConnection connection = GetConnection())
             {
@@ -42,6 +42,86 @@ namespace WPFApp.Repositories
                         return bloodTypeGraphData;
                     }
                 }
+            }
+        }
+
+        public List<BloodTypeCount> CountBlood(DateTime startDate, DateTime endDate)
+        {
+            using (SqlConnection connection = GetConnection())
+            {
+                string query = @"SELECT DonatedBlood, ReceivedBlood, Receipt_BloodType
+                         FROM dbo.countblood(@startdate, @enddate)";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@startdate", startDate);
+                    command.Parameters.AddWithValue("@enddate", endDate);
+
+                    List<BloodTypeCount> bloodTypeCounts = new List<BloodTypeCount>();
+
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        BloodTypeCount bloodTypeCount = new BloodTypeCount
+                        {
+                            DonatedBlood = reader.GetInt32(0),
+                            ReceivedBlood = reader.GetInt32(1),
+                            BloodType = reader.GetString(2)
+                        };
+
+                        bloodTypeCounts.Add(bloodTypeCount);
+                    }
+
+                    return bloodTypeCounts;
+                }
+            }
+        }
+
+        public DataTable GetGenderCounts()
+        {
+            using (SqlConnection connection = GetConnection())
+            {
+                string query = @"SELECT * FROM dbo.GetGenderCounts()";
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    SqlDataAdapter adapter = new SqlDataAdapter(command);
+                    DataTable dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+                    return dataTable;
+                }
+            }
+        }
+
+        public int CountPendingAppointments(DateTime startDate, DateTime endDate)
+        {
+            using (SqlConnection connection = GetConnection())
+            {
+                string query = "SELECT [dbo].[CountPendingAppointments](@startDate, @endDate)";
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@startDate", startDate);
+                command.Parameters.AddWithValue("@endDate", endDate);
+
+                connection.Open();
+                int count = (int)command.ExecuteScalar();
+
+                return count;
+            }
+        }
+
+        public int CountPendingRequests(DateTime startDate, DateTime endDate)
+        {
+            using (SqlConnection connection = GetConnection())
+            {
+                string query = "SELECT [dbo].[CountPendingRequests](@startDate, @endDate)";
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@startDate", startDate);
+                command.Parameters.AddWithValue("@endDate", endDate);
+
+                connection.Open();
+                int count = (int)command.ExecuteScalar();
+
+                return count;
             }
         }
 
--- ViewModels/HomeViewModel.cs
+++ ViewModels/HomeViewModel.cs
@@ -2,6 +2,7 @@
 using LiveCharts.Wpf;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using WPFApp.Models;
 using WPFApp.Repositories;
@@ -18,6 +19,8 @@ namespace WPFApp.ViewModels
 
             SeriesCollection = new SeriesCollection();
             Labels = new List<string>();
+            BloodTypeCount = new ObservableCollection<BloodTypeCount>();
+            GenderSeriesCollection = new SeriesCollection();
 
             DateTime startDate = new DateTime(2023, 01, 01);
             DateTime endDate = new DateTime(2023, 12, 31);
@@ -54,10 +57,63 @@ namespace WPFApp.ViewModels
             Labels = graphDates.Select(date => date.ToString("dd/MM/yyyy")).ToList();
 
             YFormatter = value => value.ToString("N0");
+
+            // Call the method in HomeRepository to get the blood type count data
+            List<BloodTypeCount> bloodTypeCounts = homeRepository.CountBlood(startDate, endDate);
+            BloodTypeCount = new ObservableCollection<BloodTypeCount>(bloodTypeCounts);
+
+            // Call the method in HomeRepository to get the gender count data
+            var genderCounts = homeRepository.GetGenderCounts();
+            if (genderCounts.Rows.Count > 0)
+            {
+                MaleDonorsCount = new ChartValues<int> { Convert.ToInt32(genderCounts.Rows[0]["MaleDonors"]) };
+                FemaleDonorsCount = new ChartValues<int> { Convert.ToInt32(genderCounts.Rows[0]["FemaleDonors"]) };
+                MalePatientsCount = new ChartValues<int> { Convert.ToInt32(genderCounts.Rows[0]["MalePatients"]) };
+                FemalePatientsCount = new ChartValues<int> { Convert.ToInt32(genderCounts.Rows[0]["FemalePatients"]) };
+            }
+
+            MaleDonorsLabelPoint = point => $"Male Donors: {point.Y}";
+            FemaleDonorsLabelPoint = point => $"Female Donors: {point.Y}";
+            MalePatientsLabelPoint = point => $"Male Patients: {point.Y}";
+            FemalePatientsLabelPoint = point => $"Female Patients: {point.Y}";
+
+            GenderSeriesCollection.AddRange(new SeriesCollection
+            {
+                new PieSeries { Title = "Male Donors", Values = MaleDonorsCount, DataLabels = true, LabelPoint = MaleDonorsLabelPoint, PushOut = 10 },
+                new PieSeries { Title = "Female Donors", Values = FemaleDonorsCount, DataLabels = true, LabelPoint = FemaleDonorsLabelPoint, PushOut = 10 },
+                new PieSeries { Title = "Male Patients", Values = MalePatientsCount, DataLabels = true, LabelPoint = MalePatientsLabelPoint, PushOut = 10 },
+                new PieSeries { Title = "Female Patients", Values = FemalePatientsCount, DataLabels = true, LabelPoint = FemalePatientsLabelPoint, PushOut = 10 }
+            });
+
+
+            // Call the method in HomeRepository to get the count of pending appointments
+            int pendingAppointmentCount = homeRepository.CountPendingAppointments(startDate, endDate);
+
+            // Call the method in HomeRepository to get the count of pending requests
+            int pendingRequestCount = homeRepository.CountPendingRequests(startDate, endDate);
+
+            // Update the text block bindings with the counts
+            PendingAppointmentCount = pendingAppointmentCount.ToString();
+            PendingRequestCount = pendingRequestCount.ToString();
+
         }
 
         public SeriesCollection SeriesCollection { get; set; }
         public List<string> Labels { get; set; }
         public Func<double, string> YFormatter { get; set; }
+        public ObservableCollection<BloodTypeCount> BloodTypeCount { get; set; }
+
+        public SeriesCollection GenderSeriesCollection { get; set; }
+        public ChartValues<int> MaleDonorsCount { get; set; }
+        public ChartValues<int> FemaleDonorsCount { get; set; }
+        public ChartValues<int> MalePatientsCount { get; set; }
+        public ChartValues<int> FemalePatientsCount { get; set; }
+        public Func<ChartPoint, string> MaleDonorsLabelPoint { get; set; }
+        public Func<ChartPoint, string> FemaleDonorsLabelPoint { get; set; }
+        public Func<ChartPoint, string> MalePatientsLabelPoint { get; set; }
+        public Func<ChartPoint, string> FemalePatientsLabelPoint { get; set; }
+
+        public string PendingAppointmentCount { get; set; }
+        public string PendingRequestCount { get; set; }
     }
 }
